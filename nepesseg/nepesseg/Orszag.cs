86a05a85959963e5e510b3ccba2fdd131957da5f
Veller Árpád <vellera@kkszki.hu>
2024-09-21 11:06:06 +0200

    fájl bevitel, g- átalakítás

---
+++ nepesseg/nepesseg/Orszag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    internal class Orszag
+    {
+        public string orszag;
+        public int terulet;
+        public string nepesseg;
+        public string fovaros;
+        public int fovaros_nepessege;
+
+        public Orszag(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            this.orszag = darabok[0]; 
+            this.terulet = Convert.ToInt32(darabok[1]);
+            this.nepesseg = darabok[2];
+            this.fovaros = darabok[3];
+            this.fovaros_nepessege = Convert.ToInt32(darabok[4]);
+        }
+    }
+}
--- nepesseg/nepesseg/Program.cs
+++ nepesseg/nepesseg/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,42 @@ namespace nepesseg
     {
         static void Main(string[] args)
         {
+
+            List<Orszag> adatok = new List<Orszag>();
+            StreamReader sr = new StreamReader("adatok-utf8.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                adatok.Add(new Orszag(sr.ReadLine()));
+            }
+            sr.Close();
+
+
+
+
+            for (int i = 0; i < adatok.Count; i++)
+            {
+             
+                if (adatok[i].nepesseg.EndsWith("g"))
+                {
+                    
+                    string levalaszt = adatok[i].nepesseg.TrimEnd('g');
+
+                    
+                    if (int.TryParse(levalaszt, out int szam))
+                    {
+                        
+                        int szorzottszam = szam * 10000;
+
+                       
+                        adatok[i].nepesseg = szorzottszam.ToString();
+                    }
+                    
+                }
+            }
+            
+
+            Console.ReadLine();
         }
     }
 }
