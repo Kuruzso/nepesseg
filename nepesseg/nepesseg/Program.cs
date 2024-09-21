fb51208fe3e092f54534189414773f8f4d8cdb4b
Veller Árpád <vellera@kkszki.hu>
2024-09-21 11:53:19 +0200

    Kína népsűrűssége

--- nepesseg/nepesseg/Program.cs
+++ nepesseg/nepesseg/Program.cs
@@ -46,8 +46,19 @@ namespace nepesseg
             }
             //3.Feladat
             Console.WriteLine("3.Feladat");
-            Console.WriteLine("A beolvasott országok száma: "+adatok.Count());
-            
+            Console.WriteLine("A beolvasott országok száma {0}. ",adatok.Count());
+            Console.WriteLine();
+            //4.feladat
+            Console.WriteLine("4.Feladat");
+
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].orszag == "Kína")
+                {
+                    Console.WriteLine("Kína népsűrűssége: {0} fő/km^2",int.Parse(adatok[i].nepesseg) / adatok[i].terulet);
+                }
+            }
+
 
             Console.ReadLine();
         }
